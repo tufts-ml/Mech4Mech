a4994cb683f777d138d852eaa1ceb7beb87b5451
Kaitlin Gili <kaitlinmgili@gmail.com>
2025-10-20 15:32:50 -0400

    Quick fixes to get the code working. We now optimize for the evidence

--- src/feedback_mechanism/train_mechanism.py
+++ src/feedback_mechanism/train_mechanism.py
@@ -14,7 +14,7 @@ from utils import flatten_params, inv_softplus, use_posterior, add_variational_l
 def main():
     # --- define data path ---
     repo_root = Path(__file__).resolve().parents[2]  
-    data_dir = repo_root / "data" / "embeddings"      
+    data_dir = repo_root / "data" / "feedback_mechanism"      
     path = data_dir / "training_data.npz"
     bundle = np.load(path, allow_pickle=True)
 
@@ -30,6 +30,7 @@ def main():
     label_names = bundle["label_names"].tolist() #List of label names 
     K = 8 #Number of label classes
     D = bundle["emb_dim"] #Embedding dimension
+    print(D)
     N = len(texts)
     class_weights = torch.tensor([1/1452 , 1/5 , 1/151 , 1/53, 1/19, 1/29, 1/10, 1/2], dtype=torch.float32)
 
@@ -48,7 +49,7 @@ def main():
         torch.nn.Linear(in_features=D, out_features=K),
     ).to(device)
 
-    num_of_params = len(flatten_params(model)) # + 308,000,000 Add an additional 308 million params to represent the number of params in gemmaembed LLM.
+    num_of_params = len(flatten_params(model)) + 308000000 #Add an additional 308 million params to represent the number of params in gemmaembed LLM.
 
     model.raw_sigma = torch.nn.Parameter(inv_softplus(torch.tensor(1e-4, device=device))) # Add a single variance parameter raw_sigma of the approximate posterior q(theta |x,y,M) = N(current theta, raw_sigma^2I) that we will learn via gradient descent.
     add_variational_layers(model, model.raw_sigma) # Make all regular layers variational - have parameters that can be obtained from the approximate posterior.
@@ -65,10 +66,9 @@ def main():
 
     criterion = TemperedELBOLoss(model, likelihood, prior, kappa=num_of_params/N) #Computes the ELBO: E_q log p(y|x, theta, M) - KL(q(theta|x,y,m) || p(theta|m)).
 
-    lr_list = [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001] #List of learning rates. 
+    lr_list = [0.5, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001] #List of learning rates. 
     best_loss = float('inf') 
     evidence_dict = {} #Logs the evidence values that are associated with each learning rate. 
-    weight_decay_dict = {}
     save_path = repo_root / "src" / "feedback_mechanism" / "best_model.pt"
 
     for lr in lr_list: 
@@ -86,8 +86,8 @@ def main():
 
             logits = model(X)
             loss = criterion(logits, y, class_weights, params, len(X)) #Computes the ELBO 
-            if epoch % 50 == 0
-                print(f"Epoch {epoch} | LR {lr} | ELBO loss: {loss['loss'].item():.6}")
+            #if epoch % 50 == 0:
+            print(f"Epoch {epoch} | LR {lr} | ELBO loss: {loss['loss'].item():.6}")
             loss["loss"].backward()
             optimizer.step()
 
@@ -95,11 +95,10 @@ def main():
             best_loss = loss["loss"].item() 
             torch.save(model.state_dict(), save_path) #Only saves the model parameters with the learning rate that has the lowest ELBO. 
         evidence_dict[lr] = loss["loss"].item()
-        weight_decay_dict[lr] = prior.prior_variance
+    
 
     print("Best ELBO:", best_loss)
     print("Evidence by LR:", evidence_dict)
-    print("Prior variance by LR:", weight_decay_dict)
 
 
 if __name__ == "__main__":
